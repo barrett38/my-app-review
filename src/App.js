9dbf9514f61f343e57ec23689b2c576578637245
Barrett Anderson <barrett1614@gmail.com>
2025-05-02 09:04:21 -0500

    Adjusting App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login Page</h1>
+      <input
+        placeholder="Username"
+        id="username"
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        id="loginBtn"
+        onClick={() => {
+          if (!message.trim()) {
+            alert("Please enter username");
+          } else {
+            alert(`Hello ${message}`);
+          }
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 }
